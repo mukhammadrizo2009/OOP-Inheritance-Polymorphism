a2cce8781458bffa5b2f912604018f26ffaef8fb
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-07-16 15:35:08 +0500

    Mission Done

---
+++ task15.py
@@ -0,0 +1,67 @@
+### 15. 🎮 **Game Characters**
+
+class Charecter:
+    """_summary_
+    """
+    def __init__(self):
+        """_summary_
+        """
+        pass
+    
+class Warrior(Charecter):
+    """_summary_
+
+    Args:
+        Charecter (_type_): _description_
+    """
+    def attack(self):
+        """_summary_
+        """
+        pass
+    
+    def defend(self):
+        """_summary_
+        """
+        pass
+
+class Mage(Charecter):
+    """_summary_
+
+    Args:
+        Charecter (_type_): _description_
+    """
+    def attack(self):
+        """_summary_
+        """
+        pass
+    def defend(self):
+        """_summary_
+        """
+        pass
+    
+class Archer(Charecter):
+    """_summary_
+
+    Args:
+        Charecter (_type_): _description_
+
+    """
+    def attack(self):
+        """_summary_
+        """
+        pass
+    def defend(self):
+        """_summary_
+        """
+        pass
+    
+warrior = Warrior()
+mage = Mage()
+archer = Archer()
+
+warrior.attack()
+warrior.defend()
+mage.attack()
+mage.defend()
+archer.attack()
+archer.defend()
