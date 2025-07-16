f8b390c94c81ffaa862fe8c3a5f8cd09a74d0429
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2025-07-16 15:26:57 +0500

    Mission Done

---
+++ task14.py
@@ -0,0 +1,38 @@
+### 14. 💼 **Job Portal Accounts**
+
+
+class User:
+    """_summary_
+    """
+    def __init__(self):
+        """_summary_
+        """
+        pass
+    
+class Application(User):
+    """_summary_
+
+    Args:
+        User (_type_): _description_
+    """
+    def interact(self):
+        """_summary_
+        """
+        pass
+    
+class Employer(User):
+    """_summary_
+
+    Args:
+        User (_type_): _description_
+    """
+    def interact(self):
+        """_summary_
+        """
+        pass
+    
+employer = Employer()
+application = Application()
+
+employer.interact()
+application.interact()
